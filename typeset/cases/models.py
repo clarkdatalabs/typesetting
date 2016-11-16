941bfe9988ef71e4ca7a57674603cf26c4bc5ea4
Junhui <junhui@umich.edu>
2016-11-16 15:25:02 -0500

    Ignore secret settings

--- typeset/cases/models.py
+++ typeset/cases/models.py
@@ -12,3 +12,14 @@ class Block(models.Model):
     character = models.CharField(max_length=1)
     case = models.ForeignKey(Case, on_delete=models.CASCADE)
     count = models.IntegerField()
+
+# Should return the information of most limited chracters are:
+# No, missing x character
+# looping through all the fonts and return message for each one
+# A calculation function to display how much character left for the input
+# A message to see how much it is needed to print out the input if failed
+# Go check with the input message and how many characters left in the database, return differet meassage based on different input
+
+## Some wild cards
+# Related to how many characters are more often used in today's world
+# Heat Maps
---
+++ typeset/typeset/settings_secret.py
@@ -0,0 +1,1 @@
+SECRET_KEY = 'lhh#!s+@hokyo-d@xvyv-pgzh#kumc-afwhq3#*r*%kocc7s83'
