072d8b6edbca86073a9ea0d7744db514c6bf24dc
Junhui <junhui@umich.edu>
2016-11-23 14:58:27 -0500

    fixing error with data nor in the database, adding textarea for input

--- typeset/cases/views.py
+++ typeset/cases/views.py
@@ -8,14 +8,6 @@ def results(request):
     cases = Case.objects.all()
     for case in cases:
         case.dict = check(request.POST['typesetText'],case)
-        # if (check(request.POST['typesetText'],case)[0] == True):
-        #     case.passed = True
-        #     case.dict = [1,2,3]
-        #     case.count = check(request.POST['typesetText'],case)[1]
-        # else:
-        #     case.passed = False
-        #     case.dict = [1,2,3]
-        #     case.count = check(request.POST['typesetText'],case)[1]
 
     context = {'typesetText':request.POST['typesetText'], 'Cases':cases}
     return render(request, 'cases/results.html',context)
@@ -28,11 +20,15 @@ def check(text, case):
     for char in chars:
         try:
             count = blocks.get(character=char).count
+            if(count):
+                if (count < text.count(char)):
+                    chardict[char] = [False,text.count(char)-count]
+                if (count >= text.count(char)):
+                    chardict[char] = [True,count-text.count(char)]
         except:
             chardict[char] = [False, "No such char"]
-        if(count):
-            if (count < text.count(char)):
-                chardict[char] = [False,text.count(char)-count]
-            if (count >= text.count(char)):
-                chardict[char] = [True,count-text.count(char)]
+
     return chardict
+## Heat map
+## Print out the five least char we have
+## make the input field bigger
